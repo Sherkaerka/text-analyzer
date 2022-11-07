35bdb1df54e884c2f4c655ac0f69081f0bf1949b
Erik Lindén <erikbolinden@gmail.com>
2022-11-07 13:39:58 +0000

    adjust all texts an menus to Text Analyzer Inc CRM-system

--- run.py
+++ run.py
@@ -10,11 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('star_crm')
-
-
-# try connection to sheet
-
+SHEET = GSPREAD_CLIENT.open('text_analyzer')
 
 
 def print_menu():
@@ -34,6 +30,7 @@ def print_menu():
 
     if __name__=='__main__':
         while(True):
+            print('---Main menu---')
             print_menu_options()
             option = ''
             try:
@@ -51,7 +48,7 @@ def print_menu():
                 search_contact()
                 exit()
             elif option == 4:
-                print('Thank you for using Text Analyzer. Shutting down...')
+                print('Thank you for using Text Analyzer CRM. Shutting down...')
                 exit()
             else:
                 print('Invalid option. Please enter a number between 1 and 4.')
@@ -59,20 +56,22 @@ def print_menu():
 
 def add_contact():
     """
-    Gives the user an input to enter a website URL that will be analyzed.
+    Creates a new customer record with contact details.
     """
     print('Add new customer record')
     
-    first = input('Enter First Name: ')
     company = input('Enter Company Name: ')
+    name = input('Enter Name of contact: ')
+    email = input('Enter e-mail: ')
+    phone = input('Enter phone number: ')
     sheet1 = SHEET.worksheet('sheet1')
-    sheet1.append_row([first, company])
-    print( "Added successfully!")
+    sheet1.append_row([company, name, email, phone])
+    print( "New Customer record added successfully!")
 
 
 def print_all_contacts():
     """
-    Gives the user an input field to paste a chunk of text to be analyzed.
+    Prints all contacts in the CRM to the terminal.
     """
     sheet1 = SHEET.worksheet('sheet1')
     data = sheet1.get_all_values()
@@ -81,12 +80,12 @@ def print_all_contacts():
 
 def search_contact():
     """
-    Let user search
+    Let user search within the customer database
     """
     print('let us look what we got')
 
 """
 Run program
 """
-print("Welcome to Text Analyzer")
+print("Welcome to Text Analyzer Inc. CRM-system")
 print_menu()
