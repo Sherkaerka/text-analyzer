487d47fcb0750403110ba339c079e0a5ac7bce69
Erik Lindén <erikbolinden@gmail.com>
2022-11-16 08:22:24 +0000

    removing long lines according to pep8

--- run.py
+++ run.py
@@ -16,10 +16,10 @@ SHEET = GSPREAD_CLIENT.open('text_analyzer')
 
 def print_menu():
     """
-    Prints the menu so that the user can choose what 
-    they want to do. 
+    Prints the menu so that the user can choose what
+    they want to do.
     """
-    
+
     menu_options = {
         1: 'Add contact',
         2: 'Print all contacts',
@@ -31,10 +31,10 @@ def print_menu():
 
     def print_menu_options():
         for key in menu_options.keys():
-            print (key, '--', menu_options[key] )
+            print(key, '--', menu_options[key])
 
-    if __name__=='__main__':
-        while(True):
+    if __name__ == '__main__':
+        while (True):
             print('---Main menu---')
             print_menu_options()
             option = ''
@@ -42,7 +42,7 @@ def print_menu():
                 option = int(input('What do you want to do: \n'))
             except:
                 print('Invalid input. Please enter a number.')
-            
+
             if option == 1:
                 add_contact()
             elif option == 2:
@@ -54,7 +54,7 @@ def print_menu():
             elif option == 5:
                 remove_contact()
             elif option == 6:
-                print('Thank you for using Text Analyzer CRM. Shutting down...')
+                print('Thank you for using Text Analyzer CRM. Shutting down..')
                 exit()
             else:
                 print('Invalid option. Please enter a number between 1 and 6.')
@@ -65,7 +65,7 @@ def add_contact():
     Creates a new customer record with contact details.
     """
     print('Add new customer record')
-    
+
     company = input('Enter Company Name: \n')
     fname = input('Enter First name of contact: \n')
     lname = input('Enter Last name of contact: \n')
@@ -73,23 +73,24 @@ def add_contact():
     phone = input('Enter phone number: \n')
     sheet1 = SHEET.worksheet('sheet1')
     sheet1.append_row([company, fname, lname, email, phone])
-    print( "New Customer record added successfully!")
+    print("New Customer record added successfully!")
+
 
 def print_all_contacts():
     """
-    Tabulates and print all contacts in the CRM to 
+    Tabulates and print all contacts in the CRM to
     the terminal.
     """
     sheet1 = SHEET.worksheet('sheet1')
     data = sheet1.get_all_values()
-    
+
     print(tabulate(data))
-  
+
 
 def search_contact():
     """
-    Let user search within the customer database and 
-    returns the row or rows with a match. 
+    Let user search within the customer database and
+    returns the row or rows with a match.
     """
     search_for = input('Search for any input in the contacts: \n')
     if search_for == "":
@@ -98,11 +99,11 @@ def search_contact():
 
     sheet1 = SHEET.worksheet('sheet1')
     data = sheet1.get_all_values()
-    
+
     for list in data:
         if search_for in list:
-            print(tabulate([list], headers= ["Company","First name","Last name", "Email", "Phone"]))
-
+            print(tabulate([list], headers=["Company", "First name",
+                                            "Last name", "Email", "Phone"]))
 
     search_result = input('Do you wanna search again? y/n: \n')
 
@@ -112,15 +113,13 @@ def search_contact():
         print_menu()
 
 
-
 def edit_contact():
     """
-    Use search function based on Company name. 
-    Retrieves row number and ask for wich index user wish to edit. 
+    Use search function based on Company name.
+    Retrieves row number and ask for wich index user wish to edit.
     Ask user wich data to be adjusted and the update specific cell.
     """
-    
-    
+
     search_for = str(input('Search for Company name: \n'))
     if search_for == "":
         print('Please give input')
@@ -134,31 +133,29 @@ def edit_contact():
             company_col = sheet1.col_values(1)
             rownum = company_col.index(search_for) + 1
             row = sheet1.row_values(rownum)
-            print(rownum, row) 
+            print(rownum, row)
 
-    search_result = input('Do you wanna [s] search again or [c] continue or [x]exit? s/c/x: \n')
+    search_result = input('[s]earch again or [c]ontinue or e[x]it? s/c/x: \n')
 
     if search_result == 's':
         edit_contact()
     elif search_result == 'x':
         print_menu()
-    
 
-    edit_list = input('Enter index on record you want to edit or [x] to exit to main menu: \n')
-    
+    edit_list = input('Enter index on record you want to edit \
+        or [x] to exit to main menu: \n')
+
     if edit_list == "":
         print('Please give input')
         edit_contact()
     elif edit_list == 'x':
         print_menu()
-    
 
     actual_row = int(edit_list)
-    
-    if actual_row != rownum:    
+
+    if actual_row != rownum:
         print('You are trying to edit another record')
         print_menu()
-
 
     edit_menu = {
         1: 'Company name',
@@ -168,13 +165,13 @@ def edit_contact():
         5: 'Phone',
         6: 'Exit',
     }
-        
+
     def print_edit_menu():
         for key in edit_menu.keys():
-            print (key, '--', edit_menu[key] )
+            print(key, '--', edit_menu[key])
 
-    if __name__=='__main__':
-        while(True):
+    if __name__ == '__main__':
+        while (True):
             print('---Edit menu---')
             print_edit_menu()
             option = ''
@@ -182,7 +179,7 @@ def edit_contact():
                 option = int(input('What do you want to edit: \n'))
             except:
                 print('Invalid input. Please enter a number.')
-            
+
             if option == 1:
                 new_input = input('Please enter new input: \n')
                 col = 'A'
@@ -219,16 +216,15 @@ def edit_contact():
                 print('Phone record updated successfully')
                 print_menu()
             elif option == 6:
-                print('Thank you for using Text Analyzer CRM. Shutting down...')
+                print('Thank you for using Text Analyzer CRM. Shutting down..')
                 exit()
             else:
                 print('Invalid option. Please enter a number between 1 and 6.')
 
-    
 
 def remove_contact():
     """
-    Use search function based on Company name. Retrieves row number 
+    Use search function based on Company name. Retrieves row number
     and ask for wich index user wish to delete.
     """
 
@@ -245,28 +241,27 @@ def remove_contact():
             company_col = sheet1.col_values(1)
             rownum = company_col.index(search_for) + 1
             row = sheet1.row_values(rownum)
-            print(rownum, row) 
+            print(rownum, row)
 
-    search_result = input('Do you wanna [s] search again or [c] continue or [x]exit? s/c/x: \n')
+    search_result = input('[s]earch again or [c]ontinue or e[x]it? s/c/x: \n')
 
     if search_result == 's':
         remove_contact()
     elif search_result == 'x':
         print_menu()
-    
 
-    edit_list = input('Enter index on record you want to remove or [x] to exit to main menu: \n')
-    
+    edit_list = input('Enter index on record you want to remove or \
+         [x] to exit to main menu: \n')
+
     if edit_list == "":
         print('Please give input')
         remove_contact()
     elif edit_list == 'x':
         print_menu()
-    
 
     actual_row = int(edit_list)
-    
-    if actual_row != rownum:    
+
+    if actual_row != rownum:
         print('You are trying to edit another record')
         print_menu()
     elif actual_row == rownum:
@@ -276,7 +271,7 @@ def remove_contact():
         print_menu()
     else:
         print('You are trying to remove another record')
-     
+
 
 """
 Run program
